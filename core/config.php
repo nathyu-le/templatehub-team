f603eb3964cff776fae77cbba3c076bc433e19b4
nguyen142hao <nguyenhao14022005@gmail.com>
2025-12-15 23:53:23 +0700

    update folder core by hào

---
+++ core/config.php
@@ -0,0 +1,8 @@
+<?php
+// core/config.php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'gtaphoatoolcom_final');
+define('DB_USER', 'gtaphoatoolcom_final');
+define('DB_PASS', 'gtaphoatoolcom_final');
+
+define('BASE_URL', '/');
